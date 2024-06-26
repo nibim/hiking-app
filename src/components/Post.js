5a6157e1fae00b86a9ec37d0195d46e1b64e54ac
nibim <nibm@kth.se>
2024-06-26 22:18:41 +0200

    fix: retrieving images for post from firestore

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import '../App.css';
+import { getStorage,getDownloadURL,ref } from "firebase/storage";
+import { useState } from "react";
+import { Outlet, Link } from "react-router-dom";
 
 export default function Post(props){
+    const [img, setImg] = useState('')
+    const storage = getStorage();
+    function readMore(){
+      <div>
+        
+      </div>
+    }
+    useEffect(()=> {
+      getDownloadURL(ref(storage, 'posts/'+ props.index + '.jpeg'))
+      .then((url) => {
+      setImg(url)
+      })
+      .catch((error) => {
+        // Handle any errors
+      });},[props.index,storage])
     return(
         <div className="single-post">
-            <img src={props.img} alt="abcd" className="post-image"/>
+            <img src={img} alt="abcd" className="post-image"/>
             <h1>{props.topic}</h1>
             <p>{props.bio}</p>
+            <Link className="button-border button" to="/About">About</Link>
+            <Outlet/>
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,48 +4,39 @@ import '../App.css';
 import { getDatabase, ref as databaseRef, child, get } from "firebase/database";
 import { useEffect, useState } from "react";
 import app from "../utils/firebase";
-import { getStorage, ref as storageRef, getDownloadURL } from "firebase/storage";
-
-
-
 
 
 export default function Home() {
     const [posts,setPosts] = useState([]);
     const dbRef = databaseRef(getDatabase(app));
-    const [imgurl, setImgurl] = useState('');
+    //const [imgurl, setImgurl] = useState('');
+    const [update, setUpdate] = useState(false);
+
+   
     
-    const storage = getStorage();
-    useEffect(()=>{
-      getDownloadURL(storageRef(storage, 'black_Jack.jpeg'))
-      .then((url) => {
-      setImgurl(url)
-      })
-      .catch((error) => {
-        // Handle any errors
-      });
-    },[imgurl])
-    get(child(dbRef, `posts`)).then((snapshot) => {
+    
+    useEffect(()=>{get(child(dbRef, `posts`)).then((snapshot) => {
       if (snapshot.exists()) {
         setPosts(snapshot.val());
+        setUpdate(false)
       } else {
         console.log("No data available");
       }
-    }).catch((error) => {
-      console.error(error);
-    });
-
+      }).catch((error) => {
+        console.error(error);
+    });},[update])
+    
     return(
         <div className="home">
-            <Banner color='black'/>
-                <div className='posts'>{posts.map((post,index) => (
+            <Banner />
+                <div className='posts'>{posts.length !==0 ? posts.map((post) => (
                     <Post
-                    img={imgurl}
-                    key={index}
+                    key={post.key}
+                    index={post.key}
                     topic={post.topic}
                     bio={post.bio}
-                    />
-                ))}
+                    /> 
+                )) : <div/>}
                 </div>
         </div> 
     )
