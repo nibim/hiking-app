f3ad81128751f1f62e0ebbd7436b49797f6c2426
nibim <nibm@kth.se>
2024-06-26 22:20:53 +0200

    fix: Show appropriate headers for logged in and logged out users

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
+import { useAuth } from "../utils/useAuth";
 
 export default function Navbar() {
+    const{user}=useAuth()
     return(
         <>
             <nav className="navbar">
                 <div className="leftnav">
-                    <Link className="button-border button" to="/">Home</Link>
+                    <Link className="button-border button" to="/Home">Home</Link>
                     <Link className="button-border button" to="/About">About</Link>
                     <Link className="button-border button" to="/Contact">Contact</Link>
                 </div>
                 <div className="rightnav">
-                <Link className="button"to="/Login">Login</Link>
+                <Link className="button"to={user===null?'/Login':'/Account'}>{user===null?'Login':'Account'}</Link>
                 </div>
             </nav>
             <Outlet />
