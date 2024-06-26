353e2342a86ba58f0eee5ed1642e9b77aac23d0e
nibim <nibm@kth.se>
2024-06-26 22:24:28 +0200

    fix: Change routes and add routes for account screen

--- src/App.js
+++ src/App.js
@@ -6,21 +6,28 @@ import Home from './pages/Home';
 import Contact from './pages/Contact';
 import About from './pages/About';
 import Login from './pages/Login';
-
+import { AuthContext } from './utils/Authcontext';
+import { useState } from 'react';
+import Account from './pages/Account';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 function App() {
+  const [user, setUser] = useState(null)
   return (
     <div className="App">
+      <AuthContext.Provider value={{ user, setUser }}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Navbar />}>
-            <Route index element={<Home />} />
+            <Route path='Home' element={<Home />} />
             <Route path="About" element={<About />} />
             <Route path="Contact" element={<Contact />} />
             <Route path="Login" element={<Login />} />
+            <Route path="Account" element={<Account />}/>
           </Route>
         </Routes>
       </BrowserRouter> 
+      </AuthContext.Provider>
     </div>
   );
 }
