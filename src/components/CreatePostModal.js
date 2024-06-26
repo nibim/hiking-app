ba712b284c785deee1bc78930d8cd037541a878c
nibim <nibm@kth.se>
2024-06-26 22:22:01 +0200

    feat: add modal for creation of posts

---
+++ src/components/CreatePostModal.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { useState,useEffect } from "react";
+import { getDatabase, ref as dataRef, update, child, get  } from "firebase/database";
+import { getStorage, ref as storeRef, uploadBytes } from "firebase/storage";
+
+
+
+
+export default function CreatePostModal(params) {
+  const [index,setIndex]= useState(0);
+  
+    const [bio ,setBio] = useState('');
+    const [topic ,setTopic] = useState('');
+    const [reload, setReload] = useState(false)
+    const [selectedFile, setSelectedFile] = useState(null);
+    const storage = getStorage();
+    const db = getDatabase()
+    const dbRef = dataRef(db)
+    const storageRef = storeRef(storage, 'posts/' + index + ".jpeg");
+
+    useEffect(()=>{
+      setReload(false)
+      get(child(dbRef, `posts`)).then((snapshot) => {
+      if (snapshot.exists()) {
+        const snap = snapshot.val()
+        snap.length === 0 ? setIndex(0) : setIndex(snap[snap.length-1].key+1)
+      } else {
+        console.log("No data available");
+      }
+      }).catch((error) => {
+        console.error(error);
+    });},[reload])
+  const onUpload = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+
+
+    function onAddPost() {
+        const db = getDatabase();
+        const value = {
+            bio: bio,
+            topic: topic,
+            key: index
+          };
+        update(dataRef(db,`posts`),{
+            [index] : value
+        } );
+        uploadBytes(storageRef, selectedFile, {contentType: "image/jpeg"}).then((snapshot) => {
+          setReload(true)
+          });
+       
+    }
+    return(
+        <div>
+               <div className="loginScreen">
+                  
+                    <div className="login">
+                        <h1 className="signin">Add post</h1>
+                        <input className="inputContent" value={bio} type="text" placeholder="Bio" onChange={(ev) => setBio(ev.target.value)} /> 
+                        <input className="inputContent" value={topic} type="text" placeholder="Topic" onChange={(ev)=> setTopic(ev.target.value)}/>
+                        <input className={'inputButton'} type="file" onChange={onUpload} accept="image/*"/>
+                        <input className={'inputButton'} type="button" onClick={onAddPost} value={'Add post'} />
+                    </div>
+                    
+                </div>
+        
+            
+            
+        </div>
+    )
+}
