f9b35688e4bb88508069401cb44a0862f734de40
nibim <nibm@kth.se>
2024-06-27 22:41:23 +0200

    page: contacts done except email linking

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,35 @@
 import Banner from "../components/Banner"
 import '../App.css';
+import instaImg from "../instagram.png"
+import emailImg from "../email.png"
+import ButtonMailto from "../components/ButtonMailto" 
+import { Link } from "react-router-dom";
+
 
 export default function Contact() {
+    function instagramHandler(params) {
+        window.location.href="https://www.instagram.com/hiking_sweden/"
+    }
+    function emailHandler() {
+        
+    }
     return(
         <div>
-        <Banner  />
+            <Banner  />
+            <div>
+                <h1>Contact Us</h1>
+                <div className="contacts">
+                    <div onClick={instagramHandler}>
+                        <img src={instaImg} alt='Instagram Icon'/>
+                        <h5>Instagram</h5>
+                    </div>
+                    <div onClick={() => {window.location = 'mailto:beignima8@gmail.com'; console.log("Nima")}}>
+                        <img src={emailImg} alt='Email Icon'/>
+                        <h5>Email</h5>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     )
 }
